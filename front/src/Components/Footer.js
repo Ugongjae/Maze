cc19018402568dad2fdb4a9580b9cbc9a6454400
Ugongjae <turtle0191@naver.com>
2020-04-28 00:38:45 +0900

    move to directory

---
+++ front/src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div id="footer">
+                여기는 footer
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ front/src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/header.css';
+
+class Header extends React.Component{
+    render(){
+        return(
+            <div id="header">
+                여기는 헤더
+            </div>
+        );
+    }
+}
+
+export default Header;
