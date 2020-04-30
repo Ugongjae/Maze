28c6870843e552f8f90933911b3201869d163524
Ugongjae <turtle0191@naver.com>
2020-05-01 00:09:53 +0900

    split repository

---
+++ src/Components/GNB.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class GNB extends React.Component{
+    render(){
+        return(
+            <nav>
+                네비바
+            </nav>
+        );
+    }
+}
+
+export default GNB;
